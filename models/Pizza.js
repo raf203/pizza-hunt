ffc6553e402ac79467eec87aac60a2ea87f0a6c4
Rafael Cardoso <rrafa203@gmail.com>
2022-01-20 17:34:41 -0800

    add validation

--- models/Pizza.js
+++ models/Pizza.js
@@ -2,12 +2,16 @@ const dateFormat = require('../utils/dateFormat');
 const { Schema, model } = require('mongoose');
 
 const PizzaSchema = new Schema({
-    pizzaName: {
-      type: String
-    },
-    createdBy: {
-      type: String
-    },
+  pizzaName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  createdBy: {
+    type: String,
+    required: true,
+    trim: true
+  },
     createdAt: {
       type: Date,
       default: Date.now,
@@ -15,6 +19,8 @@ const PizzaSchema = new Schema({
     },
     size: {
       type: String,
+      required: true,
+      enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
       default: 'Large'
     },
     toppings: [],
